c43ddae8bde8a6491dcf581358ab87c5d08c28f0
Olivier Papineau <olivier.12.papineau@gmail.com>
2020-10-10 23:11:36 -0400

    Setup classe Scraper pour get les categories

--- modules/Scraper.py
+++ modules/Scraper.py
@@ -1,5 +1,20 @@
+import requests
+import json
+from bs4 import BeautifulSoup as bSoup
+
+URLS = {
+    'en': 'https://www.saq.com/en/products',
+    'fr': 'https://www.saq.com/fr/produits'
+}
+
 class Scraper:
     def __init__(self, language):
         self.site_language = language
+        self.url = URLS[language]
+
+
+    def get_categories(self):
+        page = requests.get(self.url)
+        
 
     # def get_categories(self)
