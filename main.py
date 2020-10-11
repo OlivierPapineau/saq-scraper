cdf961ded93ba42dfc26ca7be6987ae7cdf6f23e
Olivier Papineau <olivier.12.papineau@gmail.com>
2020-10-10 23:03:44 -0400

    Echaffaudage de la classe Program + structure de base

---
+++ main.py
@@ -0,0 +1,28 @@
+LANGUAGES = ('fr', 'en')
+
+class Program:
+   def __init__(self):
+      self.init_message = '|  SAQ Web Scraper - Olivier Papineau | 2020 Tout droits réservés'
+      self.language = 'fr'
+
+      self.print_intro()
+      self.choose_language()
+
+      print(self.language)
+
+
+   def print_intro(self):
+      print('*' + '-' * (len(self.init_message) + 1) + '*')
+      print(self.init_message + '  |')
+      print('*' + '-' * (len(self.init_message) + 1) + '*')
+
+
+   def choose_language(self):
+      self.language = input(f'Choisir la langue de collecte {LANGUAGES} : ')
+
+      if self.language not in LANGUAGES:
+         print(f'Veuillez choisir une langue parmis les choix. {LANGUAGES}\n')
+         self.choose_language()
+
+
+p = Program()
---
+++ modules/Scraper.py
@@ -0,0 +1,5 @@
+class Scraper:
+    def __init__(self, language):
+        self.site_language = language
+
+    # def get_categories(self)
