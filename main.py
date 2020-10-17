b6b58277683bf87c7bcbf3e6a2f9f30100d9fcce
Olivier Papineau <olivier.12.papineau@gmail.com>
2020-10-17 15:30:24 -0400

    Enumeration des categories

--- main.py
+++ main.py
@@ -1,28 +1,54 @@
+from modules.Scraper import Scraper
+
 LANGUAGES = ('fr', 'en')
+LOCALE_TEXTS = {
+    'categories': {
+        'en': 'Categories',
+        'fr': 'Catégories',
+    }
+}
+
 
 class Program:
-   def __init__(self):
-      self.init_message = '|  SAQ Web Scraper - Olivier Papineau | 2020 Tout droits réservés'
-      self.language = 'fr'
+    def __init__(self):
+        self.init_message = '|  SAQ Web Scraper - Olivier Papineau | 2020 Tout droits réservés'
+        self.language = 'fr'
+        self.category = None
 
-      self.print_intro()
-      self.choose_language()
+        # Choosing language
+        self.print_intro()
+        self.choose_language()
 
-      print(self.language)
+        # Getting categories from scraper
+        self.scraper = Scraper(self.language)
+        self.categories = self.scraper.get_categories()
 
+        # Choosing category
+        self.choose_category()
 
-   def print_intro(self):
-      print('*' + '-' * (len(self.init_message) + 1) + '*')
-      print(self.init_message + '  |')
-      print('*' + '-' * (len(self.init_message) + 1) + '*')
+    def print_intro(self):
+        print('*' + '-' * (len(self.init_message) + 1) + '*')
+        print(self.init_message + '  |')
+        print('*' + '-' * (len(self.init_message) + 1) + '*')
 
+    def choose_language(self):
+        self.language = input(f'Choisir la langue de collecte {LANGUAGES} : ')
 
-   def choose_language(self):
-      self.language = input(f'Choisir la langue de collecte {LANGUAGES} : ')
+        if self.language not in LANGUAGES:
+            print(
+                f'Veuillez choisir une langue parmis les choix. {LANGUAGES}\n')
+            self.choose_language()
 
-      if self.language not in LANGUAGES:
-         print(f'Veuillez choisir une langue parmis les choix. {LANGUAGES}\n')
-         self.choose_language()
+    def choose_category(self):
+        categories = list(
+            map(lambda item: item['category'], self.categories)
+        )
+
+        print('\n')
+        print(LOCALE_TEXTS['categories'][self.language] + '\n')
+
+        for i, category in enumerate(categories):
+            print(f'[{i}] {category}')
 
 
 p = Program()
--- modules/Scraper.py
+++ modules/Scraper.py
@@ -7,14 +7,32 @@ URLS = {
     'fr': 'https://www.saq.com/fr/produits'
 }
 
+
 class Scraper:
     def __init__(self, language):
         self.site_language = language
         self.url = URLS[language]
+        self.__categories = []
 
+        self.fetch_categories()
+
+    def fetch_categories(self):
+        page = requests.get(self.url)
+        soup = bSoup(page.content, 'html.parser')
+
+        product_type_elements = soup.find_all("a", "gtm-cat-filters")
+
+        for product in product_type_elements:
+            category = product.text.strip().split('\n')[0].strip()
+
+            href = product['href']
+
+            self.__categories.append({
+                'category': category,
+                'href': href
+            })
+
+    # GETTERS / SETTERS
 
     def get_categories(self):
-        page = requests.get(self.url)
-        
-
-    # def get_categories(self)
+        return self.__categories
